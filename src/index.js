7ca52fcd142ae366c54168ed768c036f51891da0
furyabck <furyabck@gmail.com>
2023-01-31 17:50:51 +0545

    random changes

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import reportWebVitals from './reportWebVitals';
 import 'tachyons';
 import App from './App'
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
